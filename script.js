c423655eac36355b8e161711e0ec5bee908e0299
Lê Nguyễn Nhật Thành <lenguyennhatthanh72@gmail.com>
2025-09-25 20:31:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll(".image");
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        const videoId = image.getAttribute("data-id");
+        const url_video = `https://www.youtube.com/embed/${videoId}`;
+        document.querySelector("iframe").src = url_video;
+    });
+});
+
+async function getData() {
+    try {
+        const response = await fetch("https://quickshow-server.vercel.app/api/show/all"); // Gửi yêu cầu GET
+        if (!response.ok) throw new Error("Lỗi mạng hoặc API"); // Kiểm tra lỗi
+        const data = await response.json(); // Chuyển phản hồi thành JSON
+        console.log(data); // Xử lý dữ liệu
+    } catch (error) {
+        console.error("Lỗi:", error.message); // Xử lý lỗi
+    }
+}
+getData();
